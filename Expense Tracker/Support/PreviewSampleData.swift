bdf8f0fc24956b62fdf52463edb35f8d910913a0
sivabalanams <sivabalanams@gmail.com>
2023-09-14 14:43:22 +0530

    Preview sample data fixes

--- Expense Tracker/Support/PreviewSampleData.swift
+++ Expense Tracker/Support/PreviewSampleData.swift
@@ -15,20 +15,37 @@ let previewContainer: ModelContainer = {
         let container = try ModelContainer(
             for: ETTransaction.self, ETMonthCashFlow.self,
             configurations: ModelConfiguration(isStoredInMemoryOnly: true))
-        container.mainContext.insert(ETTransaction(amount: 116000, date: .now, type: .credit, category: .salary, group: .income, comment: "My salary"))
-        container.mainContext.insert(ETTransaction(amount: 72184.16, date: .now, type: .credit, category: .salary, group: .income, comment: "Wife salary"))
-        container.mainContext.insert(ETTransaction(amount: 9500, date: .now, type: .debit, category: .houseRent, group: .home))
-        container.mainContext.insert(ETTransaction(amount: 800, date: .now, type: .debit, category: .houseMaintenance, group: .home))
-        container.mainContext.insert(ETTransaction(amount: 90, date: .now, type: .debit, category: .milk, group: .provisions))
-        container.mainContext.insert(ETTransaction(amount: 1400, date: .now, type: .debit, category: .groceries, group: .provisions))
-        container.mainContext.insert(ETTransaction(amount: 350, date: .now, type: .debit, category: .snacksBeverages, group: .provisions))
-        container.mainContext.insert(ETTransaction(amount: 199, date: .now, type: .debit, category: .netflix, group: .entertainment))
-        container.mainContext.insert(ETTransaction(amount: 1178.82, date: .now, type: .debit, category: .broadbandJio, group: .utilities))
-        container.mainContext.insert(ETTransaction(amount: 2895, date: .now, type: .debit, category: .vehicleMaintenance, group: .transportation))
-        container.mainContext.insert(ETTransaction(amount: 600, date: .now, type: .debit, category: .oilChanges, group: .transportation))
         
-        let monthCashflow: ETMonthCashFlow = ETMonthCashFlow(id: Date().formatExpenseMonth(), income: 116918.20, expenses: 45612.90, budget: 50000)
-        container.mainContext.insert(monthCashflow)
+        let today: Date = .now
+        let previousMonth: Date = Calendar.current.date(byAdding: .month, value: -1, to: today)!
+        let transactions: [ETTransaction] = [
+            ETTransaction(amount: 116000, date: previousMonth, type: .credit, category: .salary, group: .income, comment: "My salary"),
+            ETTransaction(amount: 116000, date: today.addingTimeInterval(1 * 60), type: .credit, category: .salary, group: .income, comment: "My salary"),
+            ETTransaction(amount: 72184.16, date: today.addingTimeInterval(2 * 60), type: .credit, category: .salary, group: .income, comment: "Wife salary"),
+            ETTransaction(amount: 9500, date: today.addingTimeInterval(3 * 60), type: .debit, category: .houseRent, group: .home),
+            ETTransaction(amount: 800, date: today.addingTimeInterval(4 * 60), type: .debit, category: .houseMaintenance, group: .home),
+            ETTransaction(amount: 90, date: today.addingTimeInterval(5 * 60), type: .debit, category: .milk, group: .provisions),
+            ETTransaction(amount: 1400, date: today.addingTimeInterval(6 * 60), type: .debit, category: .groceries, group: .provisions),
+            ETTransaction(amount: 350, date: today.addingTimeInterval(7 * 60), type: .debit, category: .snacksBeverages, group: .provisions),
+            ETTransaction(amount: 199, date: today.addingTimeInterval(8 * 60), type: .debit, category: .netflix, group: .entertainment),
+            ETTransaction(amount: 1178.82, date: today.addingTimeInterval(9 * 60), type: .debit, category: .broadbandJio, group: .utilities),
+            ETTransaction(amount: 2895, date: today.addingTimeInterval(10 * 60), type: .debit, category: .vehicleMaintenance, group: .transportation),
+            ETTransaction(amount: 600, date: today.addingTimeInterval(11 * 60), type: .debit, category: .oilChanges, group: .transportation)
+        ]
+        
+        for transaction in transactions {
+            container.mainContext.insert(transaction)
+            
+            let expenseMonthId = transaction.date.formatExpenseMonth()
+            var monthCashFlows = try? container.mainContext.fetch(FetchDescriptor<ETMonthCashFlow>(predicate: #Predicate<ETMonthCashFlow> { $0.id == expenseMonthId }))
+            var monthCashFlow = monthCashFlows?.last ?? ETMonthCashFlow(id: expenseMonthId, income: 0, expenses: 0)
+            if  transaction.type == .credit {
+                monthCashFlow.income = monthCashFlow.income + transaction.amount
+            } else {
+                monthCashFlow.expenses = monthCashFlow.expenses + transaction.amount
+            }
+            container.mainContext.insert(monthCashFlow)
+        }
         
         return container
     } catch {
